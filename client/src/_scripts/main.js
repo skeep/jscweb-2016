b2c06bfe885dbe2b0c4d0ba4cc8ea93e1f7f2fc6
Rahul Gaba <rahulgaba16@gmail.com>
2015-12-30 17:11:03 +0530

    teaser page modifications

--- client/src/_scripts/main.js
+++ client/src/_scripts/main.js
@@ -9,4 +9,12 @@ import Link from '../_modules/link/link';
 $(() => {
   new Link(); // Activate Link modules logic
   console.log('Welcome to Yeogurt!');
+  $('.email-submit').click(function() {
+    if($'.email-input').value) {
+      var firebaseRef = new Firebase("https://jscweb2016.firebaseio.com/");
+      firebaseRef.push({
+        "email": $'.email-input').value
+      });
+    }
+  });
 });
