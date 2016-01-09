4886a20220c09b242bbf4109254f771b1bc97a78
Rahul Gaba <rahulgaba16@gmail.com>
2016-01-09 13:58:22 +0530

    email save notification

--- client/src/_scripts/main.js
+++ client/src/_scripts/main.js
@@ -9,11 +9,23 @@ import Link from '../_modules/link/link';
 $(() => {
   new Link(); // Activate Link modules logic
   console.log('Welcome to Yeogurt!');
+  var firebaseRef = new Firebase("https://jscweb2016.firebaseio.com/");
+
   $('.email-submit').click(function() {
-    if(document.querySelector('.email-input').value) {
-      var firebaseRef = new Firebase("https://jscweb2016.firebaseio.com/");
+    $('.email-input').value = "abc";
+
+    if (document.querySelector('.email-input').value) {
+      $('.email-input, .email-submit').prop("disabled", true);
       firebaseRef.push({
         "email": document.querySelector('.email-input').value
+      }, function() {
+        document.querySelector('.email-input').value = "";
+        document.querySelector('.notification-txt').style.display = "block";
+        window.setTimeout(function () {
+          document.querySelector('.notification-txt').style.display = "none";
+        },1000);
+        console.log('email saved');
+        $('.email-input, .email-submit').prop("disabled", false);
       });
     }
   });
