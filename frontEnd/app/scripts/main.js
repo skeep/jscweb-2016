c3ea4306293cf562138fbed36a7db0a8eb1b4d84
Rahul Gaba <rahulgaba16@gmail.com>
2016-02-10 01:53:21 +0530

    added smooth scroll and fast loading of background image

--- frontEnd/app/scripts/main.js
+++ frontEnd/app/scripts/main.js
@@ -1,3 +1,28 @@
-// $(document).ready(function() {
-    // $('.body-tag').fullpage();
-// });
+$(document).ready(function() {
+  'use strict';
+  function smoothScollerInit() {
+    $(document).on('click', 'a[href^="#"]', function(e) {
+      // target element id
+      var id = $(this).attr('href');
+
+      var $id = $(id);
+      if ($id.length === 0) {
+        return;
+      }
+      e.preventDefault();
+
+      // top position relative to the document
+      var pos = $(id).offset().top;
+
+      // animated top scrolling
+      $('body, html').animate({
+        scrollTop: pos
+      }, 1000);
+    });
+  }
+  function initBackgroundImages() {
+    $('.teaser-container').backstretch('../images/background.jpg');
+  }
+  smoothScollerInit();
+  initBackgroundImages();
+});
