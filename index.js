f3129b94274c1a4f3dd4a75d7c2c14bfc01e2761
vatsalya25 <vatsalya25@gmail.com>
2016-02-10 20:56:08 +0530

    modified index.js. do npm install

--- index.js
+++ index.js
@@ -4,4 +4,13 @@ var app = express();
 app.use('/',express.static('public'));
 app.use('/admin',express.static('adminUI'));
 
+var express = require('express');
+var compress = require('compression');
+
+var app = express();
+app.use(compress());
+
+app.use('/',express.static('public'));
+app.use('/admin',express.static('adminUI'));
+
 app.listen(process.env.PORT || 5000);
