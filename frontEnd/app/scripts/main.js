24f1a506a1534229fff1a33bac612a119366af21
Charu Priya <charu_priya@mckinsey.com>
2016-03-21 13:25:46 +0530

    video added

--- frontEnd/app/scripts/main.js
+++ frontEnd/app/scripts/main.js
@@ -1,38 +1,41 @@
 $(document).ready(function() {
-  'use strict';
-  function smoothScollerInit() {
-    $(document).on('click', 'a[href^="#"]', function(e) {
-      // target element id
-      var id = $(this).attr('href');
-      
-      var $id = $(id);
-      if ($id.length === 0) {
-        return;
-      }
-      e.preventDefault();
+    'use strict';
 
-      // top position relative to the document
-      var pos = $(id).offset().top;
-      pos-=50;
-      // animated top scrolling
-      $('body, html').animate({
-        scrollTop: pos
-      }, 1000);
+    function smoothScollerInit() {
+        $(document).on('click', 'a[href^="#"]', function(e) {
+            // target element id
+            var id = $(this).attr('href');
+
+            var $id = $(id);
+            if ($id.length === 0) {
+                return;
+            }
+            e.preventDefault();
+
+            // top position relative to the document
+            var pos = $(id).offset().top;
+            pos -= 50;
+            // animated top scrolling
+            $('body, html').animate({
+                scrollTop: pos
+            }, 1000);
+        });
+    }
+
+    function initBackgroundImages() {
+        //$('.teaser-container').backstretch('../images/background.jpg');
+        $('.covervid-video').coverVid();
+    }
+    smoothScollerInit();
+    initBackgroundImages();
+
+    $('#homeView,#about,#tickets,#venue,#speakers,#gallery').click(function() {
+        $('.navbar-toggle').attr('aria-expanded', false);
+        $('.navbar-toggle').addClass('collapsed');
+        $('.navbar-collapse').attr('aria-expanded', false);
+        $('.navbar-collapse').addClass('collapse');
+        $('.navbar-collapse').removeClass('in');
     });
-  }
-  function initBackgroundImages() {
-    $('.teaser-container').backstretch('../images/background.jpg');
-  }
-  smoothScollerInit();
-  initBackgroundImages();
-
-  $('#homeView,#about,#tickets,#venue,#speakers,#gallery').click(function() {
-    $('.navbar-toggle').attr('aria-expanded', false);
-    $('.navbar-toggle').addClass('collapsed');
-    $('.navbar-collapse').attr('aria-expanded', false);
-    $('.navbar-collapse').addClass('collapse');
-    $('.navbar-collapse').removeClass('in');
-  });
 });
 
 
@@ -40,30 +43,30 @@ $(document).ready(function() {
 function initMap() {
     var drag = true;
     var width_screen = window.screen.width;
-    if(width_screen <= 768){
-      drag = false;
+    if (width_screen <= 768) {
+        drag = false;
     }
     var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 12.9679078,lng: 77.6014855},
-    zoom: 15,
-    zoomControl: true,
-    scaleControl: false,
-    scrollwheel: false,
-    disableDoubleClickZoom: true,
-    draggable: drag
-  });
-  var marker = new google.maps.Marker({
-    position: {lat: 12.9679078,lng: 77.6014855},
-    map: map,
-    icon:'../favicon.ico',
-    title: 'JSChannel Conference'
+        center: { lat: 12.9679078, lng: 77.6014855 },
+        zoom: 15,
+        zoomControl: true,
+        scaleControl: false,
+        scrollwheel: false,
+        disableDoubleClickZoom: true,
+        draggable: drag
+    });
+    var marker = new google.maps.Marker({
+        position: { lat: 12.9679078, lng: 77.6014855 },
+        map: map,
+        icon: '../favicon.ico',
+        title: 'JSChannel Conference'
 
 
-  });
-  var infoWindow = new google.maps.InfoWindow({
+    });
+    var infoWindow = new google.maps.InfoWindow({
         content: '<h4>JSChannel Conference</h4> <p class="map-bold">Ritz Carlton</p><p> No., 99, Residency Rd, Shanthala Nagar, Ashok Nagar, Bengaluru, Karnataka 560025</p> '
     });
-  google.maps.event.addListener(marker, 'click', function () {
+    google.maps.event.addListener(marker, 'click', function() {
         infoWindow.open(map, marker);
     });
 }
