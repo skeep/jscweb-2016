18011f2f7b5146cf3490e89e8e6e2f5db12d5373
Charu Priya <charu_priya@mckinsey.com>
2016-04-21 13:33:23 +0530

    handled for phones

--- frontEnd/app/scripts/main.js
+++ frontEnd/app/scripts/main.js
@@ -34,7 +34,32 @@ $(document).ready(function() {
 
     function initBackgroundImages() {
         //$('.teaser-container').backstretch('../images/background.jpg');
-        $('.covervid-video').coverVid(100, 100);
+        var isMobile = {
+            Android: function() {
+                return navigator.userAgent.match(/Android/i);
+            },
+            BlackBerry: function() {
+                return navigator.userAgent.match(/BlackBerry/i);
+            },
+            iOS: function() {
+                return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+            },
+            Opera: function() {
+                return navigator.userAgent.match(/Opera Mini/i);
+            },
+            Windows: function() {
+                return navigator.userAgent.match(/IEMobile/i);
+            },
+            any: function() {
+                return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+            }
+        };
+        if (!isMobile.any()) {
+            document.getElementsByClassName('covervid-wrapper')[0].style.display = "block";
+            document.getElementsByClassName('teaser-container')[0].style.background = "none";
+            // $('.covervid-video').coverVid(100, 100);
+            console.log("desktop");
+        }
     }
     smoothScollerInit();
     initBackgroundImages();
